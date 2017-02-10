3ddcdaa0d68ea25c26bd63ed718f36b2d6e17a33
bruno lima <brunolimadevelopment@gmail.com>
2017-02-10 19:31:34 -0300

    iniciando theme

---
+++ themes/globo/assets/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+
+	<?php wp_head(); ?>
+</head>
+<body>
+	
+<div class="container">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae earum error modi at expedita nemo laboriosam rem, maxime. Ullam praesentium cupiditate nulla eum architecto modi reprehenderit sed beatae laborum.</p>
+</div>
